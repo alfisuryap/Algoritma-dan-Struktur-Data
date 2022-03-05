807c254adaf2323bc38b5ca4e779585e788610e9
alfisuryap <alfisuryap7@gmail.com>
2022-03-05 16:55:16 +0700

    update

--- Jobsheet_3/Source_Code/Tugas/bangunRuang.java
+++ Jobsheet_3/Source_Code/Tugas/bangunRuang.java
@@ -31,6 +31,55 @@ public class bangunRuang {
             System.out.println("Volume Balok ke-" + (i + 1) + ": " + bArray[i].volume());
             System.out.println("Luas Permukaan Balok ke-" + (i + 1) + ": " + bArray[i].luasPermukaan());
         }
+
+        System.out.println();
+        System.out.print("Masukkan banyak Prisma: ");
+        int pr = sc.nextInt();
+        int a, ti, tp;
+        prisma[] prArray = new prisma[pr];
+        System.out.println("--------------------------");
+        for(int i = 0; i < prArray.length; i++){
+            System.out.print("Masukkan alas " + (i + 1) + ": ");
+            a = sc.nextInt();
+            System.out.print("Masukkan tinggi " + (i + 1) + ": ");
+            ti = sc.nextInt();
+            System.out.print("Masukkan tinggi prisma " + (i + 1) + ": ");
+            tp = sc.nextInt();
+            prArray[i] = new prisma(a, ti, tp);
+        }
+        for(int i = 0; i < prArray.length; i++){
+            System.out.println();
+            System.out.println("Prisma ke-" + (i + 1));
+            System.out.println("=============");
+            System.out.println("Alas : " + prArray[i].alas);
+            System.out.println("Tinggi : " + prArray[i].tinggiSegitiga);
+            System.out.println("Tinggi Prisma : " + prArray[i].tinggiPrisma);
+            System.out.println("Volume ke-" + (i + 1) + ": " + prArray[i].volume());
+            System.out.println("Luas Permukaan ke-" + (i + 1) + ": " + prArray[i].luasPermukaan());
+        }
+
+        System.out.println();
+        System.out.print("Masukkan banyak Tabung: ");
+        int tb = sc.nextInt();
+        int r, tt;
+        tabung[] tbArray = new tabung[tb];
+        System.out.println("--------------------------");
+        for(int i = 0; i < prArray.length; i++){
+            System.out.print("Masukkan jari-jari " + (i + 1) + ": ");
+            r = sc.nextInt();
+            System.out.print("Masukkan tinggi " + (i +1) + ": ");
+            tt = sc.nextInt();
+            tbArray[i] = new tabung(r, tt);
+        }
+        for(int i = 0; i < prArray.length; i++){
+            System.out.println();
+            System.out.println("Tabung ke-" + (i + 1));
+            System.out.println("=============");
+            System.out.println("Jari-jari : " + tbArray[i].jari);
+            System.out.println("Tinggi : " + tbArray[i].tinggiTabung);
+            System.out.println("Volume ke-" + (i + 1) + ": " + tbArray[i].volume());
+            System.out.println("Luas Permukaan ke-" + (i + 1) + ": " + tbArray[i].luasPermukaan());
+        }
+
     }
-    
 }
--- Jobsheet_3/Source_Code/Tugas/tabung.java
+++ Jobsheet_3/Source_Code/Tugas/tabung.java
@@ -1,6 +1,18 @@
 package Jobsheet_3.Source_Code.Tugas;
 
 public class tabung {
+    public int jari, tinggiTabung;
     
-    
+    public tabung(int r, int tt) {
+        jari = r;
+        tinggiTabung = tt;
+    }
+
+    public double volume(){
+        return 3.14 * jari * jari * tinggiTabung;
+    }
+
+    public double luasPermukaan(){
+        return 2 * (3.14 * jari * tinggiTabung) + 2 * (3.14 * jari * jari);
+    }
 }
