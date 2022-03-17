9f259172b83cd50b7437139dacd884d9a13af38a
alfisuryap <alfisuryap7@gmail.com>
2022-03-17 16:19:35 +0700

    update

---
+++ Jobsheet_4/Source_Code/Percobaan/Faktorial.java
@@ -0,0 +1,24 @@
+package Jobsheet_4.Source_Code.Percobaan;
+
+public class Faktorial {
+    public int nilai;
+
+    public int faktorialBF(int n) {
+        int fakto = 1;
+        for (int i = 1; i <= n; i++) {
+            fakto = fakto * i;
+        }
+        return fakto;
+    }
+
+    public int faktorialDC(int n) {
+        if (n == 1) {
+            return 1;  
+        }
+        else {
+            int fakto = n * faktorialDC(n-1);
+            return fakto;
+        }
+    }
+    
+}
---
+++ Jobsheet_4/Source_Code/Percobaan/MainSum.java
@@ -0,0 +1,30 @@
+package Jobsheet_4.Source_Code.Percobaan;
+
+import java.util.Scanner;
+
+public class MainSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("=======================================================================");
+        System.out.println("Program Menghitung Total (Satuan Juta. Misal 5.9)");
+        System.out.print("Masukan jumlah bulan : ");
+        int elm = sc.nextInt();
+
+        Sum sm = new Sum(elm);
+        System.out.println("=======================================================================");
+        for (int i = 0; i < sm.elemen; i++) {
+            System.out.print("Masukkan untung bulan ke-" + (i + 1) + " = ");
+            sm.keuntungan[i] = sc.nextDouble();
+        }
+
+        System.out.println("=======================================================================");
+        System.out.println("Algoritma Brute Force");
+        System.out.println("Total keuntungan selama " + sm.elemen + " bulan adalah = " + sm.totalBF(sm.keuntungan));
+
+        System.out.println("=======================================================================");
+        System.out.println("Algoritma Divide conquer");
+        System.out.println("Total keuntungan selama " + sm.elemen + " bulan adalah = " + sm.totalDC(sm.keuntungan, 0, sm.elemen - 1));
+    }
+
+
+}
