0142230ec8870b1ac23309ab7461e499af01220e
alfisuryap <alfisuryap7@gmail.com>
2022-04-07 14:21:00 +0700

    update

---
+++ UTS/MainMovie.java
@@ -0,0 +1,56 @@
+package UTS;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class MainMovie {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+        
+        MovieSearch data = new MovieSearch();
+        System.out.print("Masukkan jumlah movie: ");
+        int jmlM = sc.nextInt();
+        Movie[] movie = new Movie[jmlM];
+        data.movie = movie;
+        
+        for(int i = 0; i < movie.length; i++){
+            
+            int id = i + 1;
+            System.out.println("===================");
+            System.out.println("Data Movie ke-" + id);
+            System.out.println("Judul: ");
+            String judul = input.nextLine();
+            System.out.println("Tahun: ");
+            int tahun = sc.nextInt();
+            System.out.println("Rating: ");
+            float rating = sc.nextFloat();
+            
+            // instance movie
+            Movie m = new Movie(id, judul, tahun, rating);
+            data.tambah(m);
+
+            System.out.println("===================");
+        }
+        
+        System.out.println("\nDATA SEBELUM DIOLAH");
+        data.tampil();
+        
+
+        System.out.println("\nPENGURUTAN DATA");
+        data.insertionSort(true);
+        data.tampil();
+        // terapkan fungsi pengurutan data
+
+        System.out.println("\nPENCARIAN DATA");
+        System.out.println("Cari tahun:"); // isi titik-titik sesuai atribut yang diolah
+        System.out.println("Masukkan tahun Movie yang dicari:");
+        System.out.print("Tahun : ");
+        int cari = sc.nextInt(); // isi titik-titik sesuai atribut yang diolah
+        System.out.println("menggunakan Sequential Search");
+        int posisi = data.FindSeqSearch(cari);
+
+        data.tampilPosisi(cari, posisi);
+        data.tampilData(cari, posisi);
+    }
+}
---
+++ UTS/MovieSearch.java
@@ -0,0 +1,71 @@
+package UTS;
+
+public class MovieSearch {
+    Movie movie[];
+    int idx;
+
+    void tambah(Movie m){
+        if(idx < movie.length){
+            movie[idx] = m;
+            idx++;
+        }else{
+            System.out.println("Data sudah penuh !!");
+        }
+    }
+    
+    void tampil(){
+        for(Movie m : movie){
+            m.tampil();
+            System.out.println("------------------------------");
+        }
+    }
+
+    public int FindSeqSearch(int cari){
+        int posisi = -1;
+        for(int j = 0; j < movie.length; j++){
+            if(movie[j].tahun == cari){
+                posisi = j;
+                break;
+            }
+        }
+        return posisi;
+    }
+
+    public void insertionSort(boolean asc){
+        for(int i = 1; i < movie.length; i++){
+            Movie temp = movie[i];
+            int j = i;
+            if(asc){
+                while(j > 0 && movie[j - 1].tahun > temp.tahun){
+                    movie[j] = movie[j - 1];
+                    j--;
+                }
+            }else{
+                while(j > 0 && movie[j - 1].tahun > temp.tahun){
+                    movie[j] = movie[j - 1];
+                    j--;
+                }
+            }
+            movie[j] = temp;
+        }
+    }
+
+    public void tampilPosisi(int x, int  pos){
+        if(pos != -1){
+            System.out.println("data\t : " + x + " ditemukan pada indeks " + pos);
+        }else{
+            System.out.println("data " + x + "tidak ditemukan");
+        }
+    }
+
+    public void tampilData(int x, int pos){
+        if(pos != -1){
+            System.out.println("ID\t : " + movie[pos].id);
+            System.out.println("Judul\t : " + movie[pos].judul);
+            System.out.println("Tahun\t : " + x);
+            System.out.println("Rating\t : " + movie[pos].rating);
+        }else{
+            System.out.println("data " + x + "tidak ditemukan");
+        }
+    }
+}
