014e7db8c3a8caf9587cbd01553a7bc011833e68
alfisuryap <alfisuryap7@gmail.com>
2022-04-28 02:33:14 +0700

    update

--- Jobsheet_8/Source_Code/Percobaan/Praktikum1/QueueMain.java
+++ Jobsheet_8/Source_Code/Percobaan/Praktikum1/QueueMain.java
@@ -12,7 +12,7 @@ public class QueueMain {
         System.out.println("5. Clear");
         System.out.println("--------------------");
     }
-
+   
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan kapasitas queue: ");
